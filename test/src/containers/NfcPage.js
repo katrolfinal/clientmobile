86bbe171d02f39cc0c42febbca0a2b9fea189ed7
IrsantyoHadi <irsantyohadi@gmail.com>
2019-08-29 14:32:38 +0700

    final push

--- test/src/containers/NfcPage.js
+++ test/src/containers/NfcPage.js
@@ -63,95 +63,94 @@ class NfcPage extends Component {
     return (
       <View>
         <ScrollView style={{ backgroundColor: '#F2F1F2', height: Dimensions.get('window').height }}>
-          <View style={{ backgroundColor: '#374E87', height: 150, borderBottomLeftRadius: 50, borderBottomRightRadius: 50, padding: 30 }}>
-            <View style={{ alignItems: 'flex-end' }}>
-              <TouchableHighlight onPress={() => navigation.navigate('Home')} underlayColor='rgba(0,0,0,0.2)' style={{ marginRight: -20, marginTop: -20, borderRadius: 200, }}>
-                <Entypo name="cross" size={30} color="#fff" style={{}} />
-              </TouchableHighlight>
-            </View>
-            <Text style={{ fontSize: 22, fontWeight: 'bold', color: '#fff', }}>
-              This is your card!
+      <View style={{ backgroundColor: '#374E87', height: 150, borderBottomLeftRadius: 50, borderBottomRightRadius: 50, padding: 30 }}>
+        <View style={{ alignItems: 'flex-end' }}>
+          <TouchableHighlight onPress={() => closeCard()} underlayColor='rgba(0,0,0,0.2)' style={{ marginRight: -20, marginTop: -20, borderRadius: 200, }}>
+            <Entypo name="cross" size={30} color="#fff" style={{}} />
+          </TouchableHighlight>
+        </View>
+        <Text style={{ fontSize: 22, fontWeight: 'bold', color: '#fff', }}>
+          This is {employee.name.split(' ')[0]}'s card!
         </Text>
-          </View>
-          <View style={{ borderRadius: 15, backgroundColor: '#fff', shadowColor: '#000', elevation: 15, margin: 30, marginTop: -55, flexDirection: 'column' }}>
-            <View style={{ width: '100%', marginTop: 50 }}>
-              {/* IMG */}
-              <View style={{ alignItems: 'center' }}>
-
-                {
-                  employee ?
-                    <Image
-                      style={{ width: 110, height: 110, borderRadius: 200 }}
-                      source={{ uri: `${employee.image}` }}
-                    /> :
-                    <View style={{ width: 110, height: 110, borderRadius: 200, backgroundColor: 'rgba(0, 0, 0, 0.2)', justifyContent: 'center', alignItems: 'center' }}>
-                      <Text style={{ fontSize: 48, fontWeight: 'bold', color: '#FFF', marginBottom: 3 }}>{employee.name[0].toUpperCase()}</Text>
-                    </View>
-                }
-                {/* NAME & POSITION */}
-                <View style={{ marginTop: 15 }}>
-                  <View style={{ backgroundColor: 'rgba(202, 221, 250, 0.2)', borderRadius: 8, padding: 5, paddingLeft: 15, paddingRight: 15, marginTop: 5 }}>
-                    <Text style={{ color: 'rgba(0, 0, 0, 0.6)', textAlign: 'center', fontWeight: 'bold', fontSize: 24 }}>{employee.name}</Text>
-                  </View>
-                  <Text style={{ color: '#5F6DA1', fontWeight: 'bold', fontSize: 16, textAlign: 'center' }}>{employee.position}</Text>
-                  <View style={{ alignItems: 'center', marginTop: 25 }}>
-                    <QRCode
-                      size={135}
-                      value={JSON.stringify({
-                        _id: employee._id,
-                        name: employee.name,
-                        position: employee.position,
-                        phone: employee.phone,
-                        image: employee.image,
-                        company: {
-                          name: employee.company.name,
-                          color: employee.company.color
-                        },
-                        email: employee.email,
-                        showOption: false
-                      })}
-                    />
-                  </View>
+      </View>
+      <View style={{ borderRadius: 15, backgroundColor: '#fff', shadowColor: '#000', elevation: 15, margin: 30, marginTop: -55, flexDirection: 'column' }}>
+        <View style={{ width: '100%', marginTop: 30 }}>
+          {/* IMG */}
+          <View style={{ alignItems: 'center' }}>
+            {
+              employee.image ?
+                <Image
+                  style={{ width: 175, height: 175, borderRadius: 200 }}
+                  source={{ uri: `${employee.image}` }}
+                /> :
+                <View style={{ width: 175, height: 175, borderRadius: 200, backgroundColor: 'rgba(0, 0, 0, 0.2)', justifyContent: 'center', alignItems: 'center' }}>
+                  <Text style={{ fontSize: 48, fontWeight: 'bold', color: '#FFF', marginBottom: 3 }}>{employee.name[0].toUpperCase()}</Text>
                 </View>
+            }
+            {/* NAME & POSITION */}
+            <View style={{ marginTop: 15 }}>
+              <Text style={{ color: 'rgba(0, 0, 0, 0.6)', textAlign: 'center', fontWeight: 'bold', fontSize: 24 }}>{employee.name}</Text>
+              <View style={{ backgroundColor: 'rgba(202, 221, 250, 0.2)', borderRadius: 8, padding: 5, paddingLeft: 15, paddingRight: 15, marginTop: 5 }}>
+                <Text style={{ color: '#5F6DA1', fontWeight: 'bold', fontSize: 16, textAlign: 'center' }}>{employee.position}</Text>
               </View>
-              {/* PERSONAL INFORMATION */}
-              <View style={{ marginTop: 30 }}>
-
-                <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', backgroundColor: employee.company.color, padding: 20, paddingTop: 8, paddingBottom: 8, alignItems: 'center' }}>
-                  <View style={{ backgroundColor: '#fff', borderRadius: 200, padding: 10, width: 45, height: 45, justifyContent: 'center', alignItems: 'center' }}>
-                    <FontAwesome name='user' size={25} color={employee.company.color} />
-                  </View>
-                  <View>
-                    <Text style={{ color: '#FFF', textAlign: 'right' }}>0{employee.phone}</Text>
-                    <Text style={{ color: '#FFF', textAlign: 'right' }}>{employee.email}</Text>
-                  </View>
-                </View>
-
-                <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', padding: 20, paddingTop: 8, paddingBottom: 8, alignItems: 'center' }}>
-                  <View style={{ backgroundColor: employee.company.color, borderRadius: 200, padding: 10, width: 45, height: 45, justifyContent: 'center', alignItems: 'center' }}>
-                    <MaterialIcons name='location-on' size={25} color='#fff' />
-                  </View>
-                  <View>
-                    <Text style={{ color: 'rgba(0, 0, 0, 0.6)', textAlign: 'right' }}>{employee.address}</Text>
-                    {/* <Text style={{color: 'rgba(0, 0, 0, 0.6)', textAlign: 'right'}}>Jakarta Tenggara</Text> */}
-                  </View>
-                </View>
-
-                <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', backgroundColor: employee.company.color, padding: 20, paddingTop: 8, paddingBottom: 8, alignItems: 'center', borderBottomLeftRadius: 15, borderBottomRightRadius: 15 }}>
-                  <View style={{ backgroundColor: '#fff', borderRadius: 200, padding: 10, width: 45, height: 45, justifyContent: 'center', alignItems: 'center' }}>
-                    <FontAwesome5 name='link' size={25} color={employee.company.color} />
-                  </View>
-                  <View>
-                    <Text style={{ color: '#FFF', textAlign: 'right' }}>www.{employee.company.name.toLowerCase().split(' ').join(' ')}.com</Text>
-                    <Text style={{ color: '#FFF', textAlign: 'right' }}>www.{employee.company.name.toLowerCase().split(' ').join(' ')}.employee.com</Text>
-                    {/* <Text style={{color: '#FFF', textAlign: 'right'}}>www.pornhub.com</Text> */}
-                  </View>
-                </View>
-
+              <View style={{ alignItems: 'center', marginTop: 25 }}>
+                <QRCode
+                  size={120}
+                  value={JSON.stringify({
+                    _id: employee._id,
+                    name: employee.name,
+                    image: employee.image,
+                    phone: employee.phone,
+                    position: employee.position,
+                    company: {
+                      name: employee.company.name,
+                      color: employee.company.color
+                    },
+                    email: employee.email,
+                    showOption: false,
+                    _id: employee._id
+                  })}
+                />
               </View>
             </View>
           </View>
-        </ScrollView>
+          {/* PERSONAL INFORMATION */}
+          <View style={{ marginTop: 30 }}>
+
+            <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', backgroundColor: employee.company.color, padding: 20, paddingTop: 8, paddingBottom: 8, alignItems: 'center' }}>
+              <View style={{ backgroundColor: '#fff', borderRadius: 200, padding: 10, width: 45, height: 45, justifyContent: 'center', alignItems: 'center' }}>
+                <FontAwesome name='user' size={25} color={employee.company.color} />
+              </View>
+              <View>
+                <Text style={{ color: '#FFF', textAlign: 'right' }}>0{employee.phone}</Text>
+                <Text style={{ color: '#FFF', textAlign: 'right' }}>{employee.email}</Text>
+              </View>
+            </View>
+
+            <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', padding: 20, paddingTop: 8, paddingBottom: 8, alignItems: 'center' }}>
+              <View style={{ backgroundColor: employee.company.color, borderRadius: 200, padding: 10, width: 45, height: 45, justifyContent: 'center', alignItems: 'center' }}>
+                <MaterialIcons name='location-on' size={25} color='#fff' />
+              </View>
+              <View>
+                <Text style={{ color: 'rgba(0, 0, 0, 0.6)', textAlign: 'right' }}>{employee.address}</Text>
+              </View>
+            </View>
+
+            <View style={{ flexDirection: 'row', width: '100%', justifyContent: 'space-between', backgroundColor: employee.company.color, padding: 20, paddingTop: 8, paddingBottom: 8, alignItems: 'center', borderBottomLeftRadius: 15, borderBottomRightRadius: 15 }}>
+              <View style={{ backgroundColor: '#fff', borderRadius: 200, padding: 10, width: 45, height: 45, justifyContent: 'center', alignItems: 'center' }}>
+                <FontAwesome5 name='link' size={25} color={employee.company.color} />
+              </View>
+              <View>
+                <Text style={{ color: '#FFF', textAlign: 'right' }}>www.{employee.company.name.toLowerCase().split(' ').join(' ')}.com</Text>
+                <Text style={{ color: '#FFF', textAlign: 'right' }}>www.{employee.company.name.toLowerCase().split(' ').join(' ')}.employee.com</Text>
+              </View>
+            </View>
+
+          </View>
+
+        </View>
+      </View>
+    </ScrollView>
         <View style={{ height: Dimensions.get('window').height, position: 'absolute', borderRadius: 15, width: '100%' }}>
           <QRscanner navigation={this.props.navigation} style={{ alignItems: 'flex-end' }} />
         </View>
